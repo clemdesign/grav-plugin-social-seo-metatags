75fb3d9de2ebe23e2a6efe87137de9fb75a1afbe
Clement G <contact@clemdesign.fr>
2017-07-12 17:56:00 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,121 @@
+# Social & SEO Meta-Tags Plugin
+
+The **Social & SEO Meta-Tags** Plugin is for [Grav CMS](http://github.com/getgrav/grav).
+
+## Description
+
+Add and manage the SEO metatags:
+- Description
+- Robots
+- Keywords
+
+Add all Meta Tags that need Facebook Open Graph, and Twitter Cards.
+
+Note: `keyword` metatag is deprecaded for the most bots but this plugin allow management of it for the other bots.
+
+
+# Features
+
+* [Open Graph](http://ogp.me/) support.
+* [Twitter Cards](https://dev.twitter.com/cards/overview) support. You can select between Summary and Large cards.
+
+
+# Installation
+
+The command using **gpm** is not yet available.
+
+Manually installation shall be done. In `user/plugins` folder, apply the following command:
+```
+git clone https://github.com/clemdesign/grav-plugin-social-seo-metatags social-seo-metatags
+```
+
+This will clone this repository into the _social-seo-metatags_ folder.
+
+
+# Usage
+
+Plugin no need to edit any template :)
+
+After enabling plugin, for Social integration, no action to do !
+
+For SEO, you can define `robots` and `keywords` metatag content from header of markdown page. For this, add the following line:
+```md
+---
+robots: noindex, nofollow, ...
+keywords: key1, key2, key3, ...
+---
+Page content
+```
+
+
+
+# Configuration
+
+## Common SEO
+
+For SEO, you have the following default configuration:
+```
+seo:
+  robots: without
+  length: 20
+  taxonomy:
+    enabled: true
+  page_content:
+    enabled: false
+```
+
+`robots` or "Default Robots Meta-Tag" allow you to define default rules for bots. You have the following possibilities:
+- noindex: prevents the page from being indexed
+- nofollow: don't follow links from this page when looking for new pages to crawl
+- nosnippet: don't show a snippet of this page when displaying it in the search results
+- noodp: don't use text from ODP (The Open Directory Project a.k.a. dmoz.org) to generate a title or snippet for this page
+- noarchive: don't display a "Cached" link for this page in the search results
+
+`length` or "Number of keywords" allow you to define maximum number of keywords to display in meta-tag.
+
+`taxonomy.enabled` or "Use Taxonomy Keywords" allow you use Keywords of Taxonomy for Meta-Tags.
+
+`page_content.enabled` or "Use Page Content Keywords" allow you use Keywords of page content (`<strong>` and `<em>`) for Meta-Tags.
+
+## Associate Twitter account
+
+
+For Twitter, you have the following default configuration:
+```
+social_pages:
+  pages:
+    twitter:
+      enabled: true
+      type: summary
+      username: ''
+```
+
+`enabled` or "Twitter Cards active" enable integration of [Twitter Cards](https://dev.twitter.com/cards/overview) meta-tags.
+
+`type` or "Card Type" define the typê of card used by meta-tag. Accepted values are: `summary`, `summary_large_image`.
+
+`username` or "Twitter Username" is your twitter account reachable by https://twitter/username.
+
+## Associate Facebook App Id
+
+For Facebook, you have the following default configuration:
+```
+social_pages:
+  pages:
+    facebook:
+      enabled: false
+      appid: '1234567890'
+```
+
+`enabled` or "Facebook active" enable integration of [Facebook Open Graph](https://developers.facebook.com/docs/opengraph/getting-started) meta-tags.
+
+You need to generate an app_id. Without this property you will lose admin right on the Open Graph Facebook Page.
+
+# Demo
+
+Check source of the following link: [http://www.clemdesign.fr/blog/3-outils-pour-mettre-de-la-couleur-syntaxique-a-vos-articles](http://www.clemdesign.fr/blog/3-outils-pour-mettre-de-la-couleur-syntaxique-a-vos-articles)
+
+# Contributing
+
+If you think any implementation are just not the best, feel free to submit ideas and pull requests. All your comments and suggestion are welcome.
+
---
+++ social-seo-metatags.php
@@ -0,0 +1,341 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+
+/**
+ * Class SocialSEOMetaTagsPlugin
+ * @package Grav\Plugin
+ */
+class SocialSEOMetaTagsPlugin extends Plugin
+{
+
+  /** ---------------------------
+   * Private/protected properties
+   * ----------------------------
+   */
+
+  /**
+   * Instance of SocialSEOMetaTagsPlugin class
+   *
+   * @var object
+   */
+  private     $desc;
+  private     $title;
+
+  /**
+   * @return array
+   *
+   * The getSubscribedEvents() gives the core a list of events
+   *     that the plugin wants to listen to. The key of each
+   *     array section is the event that the plugin listens to
+   *     and the value (in the form of an array) contains the
+   *     callable (or function) as well as the priority. The
+   *     higher the number the higher the priority.
+   */
+  public static function getSubscribedEvents()
+  {
+
+    return [
+        'onPluginsInitialized' => ['onPluginsInitialized', 0]
+    ];
+  }
+
+
+  /**
+   * Initialize the plugin
+   */
+  public function onPluginsInitialized()
+  {
+
+    if (    !$this->isAdmin()
+        and $this->config->get('plugins.social-seo-metatags.enabled')
+        ) {
+          $this->enable([
+              'onPageInitialized'     => ['onPageInitialized', 0]
+          ]);
+        }
+  }
+
+  public function onPageInitialized(Event $e)
+  {
+    $page = $this->grav['page'];
+    //Get values
+    $meta = $page->metadata(null);
+    $this->desc = $this->sanitizeMarkdowns(strip_tags($page->summary()));
+    $this->title = $this->sanitizeMarkdowns($this->grav['page']->title());
+
+    //Pre-treament
+    if(strlen($this->desc)>160)
+    {
+      // Remove last (truncated) word and replace by ...
+      $desc_temp = substr($this->desc,0,157);
+      $this->desc = substr($desc_temp, 0, strrpos($desc_temp, ' '))."...";
+    }
+
+    //Apply change
+    $meta = $this->getSEOMetatags($meta);
+    $meta = $this->getTwitterCardMetatags($meta);
+    $meta = $this->getFacebookMetatags($meta);
+    $page->metadata($meta);
+  }
+
+  private function getSEOMetatags($meta){
+    if($this->config->get('plugins.social-seo-metatags.enabled'))
+    {
+      $page         = $this->grav["page"];
+      $page_header  = $page->header();
+      $keywords     = "";
+      /**
+       *  SEO Description
+       **/
+      $meta['description']['name']      = 'description';
+      $meta['description']['content']   = $this->desc;
+
+      /**
+       *  SEO Keywords
+       **/
+      if(isset($page_header->keywords))
+      {
+        $keywords = $page_header->keywords;
+      }
+      else
+      {
+        $length = $this->config->get('plugins.social-seo-metatags.seo.keywords.length');
+        if($length < 1) $length = 20;
+        // From Taxomany
+        if($this->config->get('plugins.social-seo-metatags.seo.keywords.taxonomy.enabled'))
+        {
+          if (array_key_exists( 'category', $page->taxonomy() )) { $categories = $page->taxonomy()['category']; } else { $categories = []; }
+          if (array_key_exists( 'tag', $page->taxonomy() )) { $tags = $page->taxonomy()['tag']; } else { $tags = []; }
+          $taxonomy = array_merge ($categories, $tags);
+          $taxonomy = array_unique ($taxonomy);
+        }
+        else
+        {
+          $taxonomy = [];
+        }
+
+        // From Page Content
+        if($this->config->get('plugins.social-seo-metatags.seo.keywords.page_content.enabled'))
+        {
+          $content = $page->getRawContent();
+          $content = str_replace("\n", " ", $content);
+          $matches = [];
+          if(preg_match_all('|<strong>(.*)</strong>|U', $content, $matches) > 0) { $strong_words = $matches[1]; } else { $strong_words = []; }
+          if(preg_match_all('|<em>(.*)</em>|U', $content, $matches) > 0) { $em_words = $matches[1]; } else { $em_words = []; }
+          $content_words = array_merge ($strong_words, $em_words);
+          $content_words = $this->cleanKeywords($content_words);
+          $content_words = array_unique ($content_words);
+        }
+        else
+        {
+          $content_words= [];
+        }
+
+        if((count($taxonomy)>0) || (count($content_words)>0))
+        {
+          $keywords_tab = array_merge ($taxonomy, $content_words);
+          $keywords_tab = array_unique ($keywords_tab);
+          $keywords_tab = array_slice($keywords_tab, 0, $length);
+          $keywords = join(',',$keywords_tab);
+        }
+      }
+
+      if($keywords != "")
+      {
+        $meta['keywords']['name']      = 'keywords';
+        $meta['keywords']['content']   = strip_tags($keywords);
+      }
+
+      /**
+       *  SEO Robots
+       **/
+      if(isset($page_header->robots))
+      {
+        $meta['robots']['name']       = 'robots';
+        $meta['robots']['content']    = $page_header->robots;
+      }
+      else
+      {
+        switch ($this->config->get('plugins.social-seo-metatags.seo.robots'))
+        {
+          case "index_follow":
+            $meta['robots']['name']       = 'robots';
+            $meta['robots']['content']    = 'index, follow';
+            break;
+          case "noindex_follow":
+            $meta['robots']['name']       = 'robots';
+            $meta['robots']['content']    = 'noindex, follow';
+            break;
+          case "index_nofollow":
+            $meta['robots']['name']       = 'robots';
+            $meta['robots']['content']    = 'index, nofollow';
+            break;
+          case "noindex_nofollow":
+            $meta['robots']['name']       = 'robots';
+            $meta['robots']['content']    = 'noindex, nofollow';
+            break;
+          case "noarchive":
+            $meta['robots']['name']       = 'robots';
+            $meta['robots']['content']    = 'noarchive';
+            break;
+          case "noodp":
+            $meta['robots']['name']       = 'robots';
+            $meta['robots']['content']    = 'noodp';
+            break;
+          case "nosnippet":
+            $meta['robots']['name']       = 'robots';
+            $meta['robots']['content']    = 'nosnippet';
+            break;
+          default:
+            // Without metatag
+            break;
+        }
+      }
+    }
+    return $meta;
+  }
+
+  private function getTwitterCardMetatags($meta){
+
+    if($this->grav['config']->get('plugins.social-seo-metatags.social_pages.pages.twitter.enabled')) {
+
+      if (!isset($meta['twitter:card'])) {
+        $meta['twitter:card']['name']      = 'twitter:card';
+        $meta['twitter:card']['property']  = 'twitter:card';
+        $meta['twitter:card']['content']   = $this->grav['config']->get('plugins.social-seo-metatags.social_pages.pages.twitter.type');
+      }
+
+      if (!isset($meta['twitter:title'])) {
+        $meta['twitter:title']['name']     = 'twitter:title';
+        $meta['twitter:title']['property'] = 'twitter:title';
+        $meta['twitter:title']['content']  = $this->title;
+      }
+
+      if (!isset($meta['twitter:description'])) {
+        $meta['twitter:description']['name']     = 'twitter:description';
+        $meta['twitter:description']['property'] = 'twitter:description';
+        $meta['twitter:description']['content']  = $this->desc;
+      }
+
+      if (!isset($meta['twitter:image'])) {
+        if (!empty($this->grav['page']->value('media.image'))) {
+          $images = $this->grav['page']->media()->images();
+          $image  = array_shift($images);
+
+          $meta['twitter:image']['name']     = 'twitter:image';
+          $meta['twitter:image']['property'] = 'twitter:image';
+          $meta['twitter:image']['content']  = $this->grav['uri']->base() . $image->url();
+        }
+      }
+
+      if (!isset($meta['twitter:site'])) {
+        //Get Twitter username
+        $user = "@".$this->grav['config']->get('plugins.social-seo-metatags.social_pages.pages.twitter.username');
+        //Update data
+        $meta['twitter:site']['name']     = 'twitter:site';
+        $meta['twitter:site']['property'] = 'twitter:site';
+        $meta['twitter:site']['content']  = $user;
+      }
+    }
+    return $meta;
+  }
+
+  private function getFacebookMetatags($meta){
+
+    if($this->grav['config']->get('plugins.social-seo-metatags.social_pages.pages.facebook.enabled')){
+
+      //Manually convert locale ll by ll_LL
+      switch($this->grav["page"]->language())
+      {
+        case "fr":
+          $locale = "fr_FR";
+          break;
+        case "en":
+          $locale = "en_EN";
+          break;
+      }
+
+      $meta['og:title']['property']       = 'og:title';
+      $meta['og:title']['content']        = $this->title;
+
+      $meta['og:description']['property'] = 'og:description';
+      $meta['og:description']['content']  = $this->desc;
+
+      $meta['og:type']['property']        = 'og:type';
+      $meta['og:type']['content']         = 'article';
+
+      $meta['og:type']['property']        = 'og:locale';
+      $meta['og:type']['content']         = $locale;
+
+      $meta['og:url']['property']         = 'og:url';
+      $meta['og:url']['content']          = $this->grav['uri']->url(true);
+
+      if (!empty($this->grav['page']->value('media.image'))) {
+        $images = $this->grav['page']->media()->images();
+        $image  = array_shift($images);
+
+        $meta['og:image']['property']  = 'og:image';
+        $meta['og:image']['content']   = $this->grav['uri']->base() . $image->url();
+      }
+
+      $meta['fb:app_id']['property']     = 'fb:app_id';
+      $meta['fb:app_id']['content']      = $this->grav['config']->get('plugins.social-seo-metatags.social_pages.pages.facebook.appid');
+
+    }
+    return $meta;
+  }
+
+  private function sanitizeMarkdowns($text){
+    $rules = array (
+        '/(#+)(.*)/'                             => '\2',  // headers
+        '/(&lt;|<)!--\n((.*|\n)*)\n--(&gt;|\>)/' => '',    // comments
+        '/(\*|-|_){3}/'                          => '',    // hr
+        '/!\[([^\[]+)\]\(([^\)]+)\)/'            => '',    // images
+        '/\[([^\[]+)\]\(([^\)]+)\)/'             => '\1',  // links
+        '/(\*\*|__)(.*?)\1/'                     => '\2',  // bold
+        '/(\*|_)(.*?)\1/'                        => '\2',  // emphasis
+        '/\~\~(.*?)\~\~/'                        => '\1',  // del
+        '/\:\"(.*?)\"\:/'                        => '\1',  // quote
+        '/```(.*)\n((.*|\n)+)\n```/'             => '\2',  // fence code
+        '/`(.*?)`/'                              => '\1',  // inline code
+        '/(\*|\+|-)(.*)/'                        => '\2',  // ul lists
+        '/\n[0-9]+\.(.*)/'                       => '\2',  // ol lists
+        '/(&gt;|\>)+(.*)/'                       => '\2',  // blockquotes
+    );
+
+    foreach ($rules as $regex => $replacement) {
+      if (is_callable ( $replacement)) {
+        $text = preg_replace_callback ($regex, $replacement, $text);
+      } else {
+        $text = preg_replace ($regex, $replacement, $text);
+      }
+    }
+
+    $text = trim($text);
+
+    return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+  }
+
+  private function cleanKeywords($array)
+  {
+    $new_array = [];
+    foreach($array as $value)
+    {
+      $push_val = true;
+      if(strlen($value) < 3)  $push_val = false;
+      if(strlen($value) > 30) $push_val = false;
+      if(preg_match("#([/|\\]{1})([0-9a-zA-Z_-]+)([.]{1})([0-9a-zA-Z_-]+)#",$value) > 0) $push_val = false;  //Remove path value
+      if(substr_count($value, ' ') > 1)  $push_val = false;                                                  //Accept only 1 space in keyword value
+
+      $value = preg_replace("/([^a-zA-Z0-9_éèêëàîïôöùûü' -]+)/", "", $value);                                //Remove all special char
+
+      if($push_val) array_push($new_array, $value);
+    }
+    return $new_array;
+  }
+
+}
