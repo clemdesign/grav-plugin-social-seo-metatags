4c2fd87a1446dd66d351b63b02c779008da09bae
David Trapp <david@dotlabs.co>
2020-10-13 15:27:05 +0200

    Fix #16 (missing hyphens)

--- social-seo-metatags.php
+++ social-seo-metatags.php
@@ -376,8 +376,8 @@ class SocialSEOMetaTagsPlugin extends Plugin
         '/\:\"(.*?)\"\:/'                        => '\1',  // quote
         '/```(.*)\n((.*|\n)+)\n```/'             => '\2',  // fence code
         '/`(.*?)`/'                              => '\1',  // inline code
-        '/(\*|\+|-)(.*)/'                        => '\2',  // ul lists
-        '/\n[0-9]+\.(.*)/'                       => '\2',  // ol lists
+        '/\n\s*(\*|\+|-)(.*)/'                   => '\2',  // ul lists
+        '/\n\s*[0-9]+\.(.*)/'                    => '\2',  // ol lists
         '/(&gt;|\>)+(.*)/'                       => '\2',  // blockquotes
     );
 
